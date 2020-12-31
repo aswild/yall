380d1b279a9a60107ba9377671b7da31bddbc3c4
Allen Wild <allenwild93@gmail.com>
2020-12-31 18:02:58 -0500

    examples: update demo to use the new quiet/verbose builder methods

--- examples/demo.rs
+++ examples/demo.rs
@@ -21,13 +21,16 @@ fn main() {
             Arg::with_name("quiet")
                 .short("q")
                 .long("quiet")
-                .help("be quiet")
+                .multiple(true)
+                .conflicts_with("verbose")
+                .help("be more quiet")
         )
         .arg(
             Arg::with_name("level")
                 .short("l")
                 .long("level")
                 .takes_value(true)
+                .conflicts_with_all(&["verbose", "quiet"])
                 .help("set the numberic level, from 0=off to 5=trace"),
         )
         .arg(
@@ -38,21 +41,20 @@ fn main() {
         )
         .get_matches();
 
-    // default level is Info, plus however many times -v was passed
-    let mut level = 3 + args.occurrences_of("verbose");
+    // create and register the logger. Most applications wouldn't have all three arguments used in
+    // this example. Here, clap is set up so that verbose, level, and quiet are all
+    // mutually-exclusive.
+    let mut logger = if let Some(l) = args.value_of("level") {
+        let level = l.parse().expect("invalid level number");
+        Logger::with_verbosity(level)
+    } else {
+        // can easily chain verbose and quiet adjustments
+        Logger::new().verbose(args.occurrences_of("verbose")).quiet(args.occurrences_of("quiet"))
+    };
 
-    // -q overrides any -v
-    if args.is_present("quiet") {
-        level = 0;
-    }
-
-    // -l overrides -q and -v
-    if let Some(l) = args.value_of("level") {
-        level = l.parse().expect("invalid level number");
-    }
-
-    // create and register the logger
-    Logger::with_verbosity(level).full_filename(args.is_present("filename")).init();
+    // Logger's builder methods pass by value instead of &mut, requiring the need to re-assign
+    logger = logger.full_filename(args.is_present("filename"));
+    logger.init();
 
     trace!("loud noises");
     debug!("squash these bugs");
