f922f3045f151e780416f3dcf231adb8c682a0a1
Allen Wild <allenwild93@gmail.com>
2023-07-13 11:28:38 -0400

    update demo example from clap v2 to clap v4

--- examples/demo.rs
+++ examples/demo.rs
@@ -3,40 +3,40 @@
 
 //! Demo of using yall::Logger with a basic `clap` app
 
-use clap::{App, Arg};
+use clap::{value_parser, Arg, ArgAction};
 use yall::log_macros::*;
 use yall::Logger;
 
 fn main() {
-    #[rustfmt::skip]
-    let args = App::new("yall example")
+    let args = clap::command!("yall example")
         .arg(
-            Arg::with_name("verbose")
-                .short("v")
+            Arg::new("verbose")
+                .short('v')
                 .long("verbose")
-                .multiple(true)
-                .help("be more verbose")
+                .action(ArgAction::Count)
+                .help("be more verbose"),
         )
         .arg(
-            Arg::with_name("quiet")
-                .short("q")
+            Arg::new("quiet")
+                .short('q')
                 .long("quiet")
-                .multiple(true)
+                .action(ArgAction::Count)
                 .conflicts_with("verbose")
-                .help("be more quiet")
+                .help("be more quiet"),
         )
         .arg(
-            Arg::with_name("level")
-                .short("l")
+            Arg::new("level")
+                .short('l')
                 .long("level")
-                .takes_value(true)
-                .conflicts_with_all(&["verbose", "quiet"])
+                .value_parser(value_parser!(u64))
+                .conflicts_with_all(["verbose", "quiet"])
                 .help("set the numberic level, from 0=off to 5=trace"),
         )
         .arg(
-            Arg::with_name("filename")
-                .short("F")
+            Arg::new("filename")
+                .short('F')
                 .long("full-filename")
+                .action(ArgAction::SetTrue)
                 .help("Show the full non-abbreviated filename in debug/trace logs"),
         )
         .get_matches();
@@ -44,16 +44,17 @@ fn main() {
     // create and register the logger. Most applications wouldn't have all three arguments used in
     // this example. Here, clap is set up so that verbose, level, and quiet are all
     // mutually-exclusive.
-    let mut logger = if let Some(l) = args.value_of("level") {
-        let level = l.parse().expect("invalid level number");
-        Logger::with_verbosity(level)
+    let mut logger = if let Some(level) = args.get_one::<u64>("level") {
+        Logger::with_verbosity(*level)
     } else {
         // can easily chain verbose and quiet adjustments
-        Logger::new().verbose(args.occurrences_of("verbose")).quiet(args.occurrences_of("quiet"))
+        Logger::new()
+            .verbose(args.get_count("verbose") as u64)
+            .quiet(args.get_count("quiet") as u64)
     };
 
     // Logger's builder methods pass by value instead of &mut, requiring the need to re-assign
-    logger = logger.full_filename(args.is_present("filename"));
+    logger = logger.full_filename(args.get_flag("filename"));
     logger.init();
 
     trace!("loud noises");
